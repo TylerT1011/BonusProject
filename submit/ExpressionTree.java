34caaf8ecab2a8b5259e29bd5714712a91f91b98
TylerT1011 <tsthomas1106@gmail.com>
2024-12-03 19:57:06 -0500

    Initial commit

---
+++ submit/ExpressionTree.java
@@ -0,0 +1,13 @@
+import itsc2214.*; // not needed now, but you might in your projects
+
+/**
+ * 
+ * @author Tyler Thomas
+ * @version December 2024
+ */
+public class ExpressionTree {
+    
+    
+
+
+}
---
+++ submit/ExpressionTreeTest.java
@@ -0,0 +1,25 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import itsc2214.*; // not needed now, but you might in your projects
+
+public class ExpressionTreeTest {
+
+    /**
+     * runner - known as the "test runner" object, this is the
+     * object that you will use in testing. You will use this
+     * object in each of your tests after reinitializing (new)
+     * in the setup() method below.
+     */
+    private ExpressionTree runner;
+
+    /**
+     * setup() method, runs before each test method below.
+     * Use this method to recreate the objects needed for
+     * testing your class.
+     */
+    @Before
+    public void setup() {
+        runner = new ExpressionTree();
+    }
+
+}
